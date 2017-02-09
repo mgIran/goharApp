b609210f29a593463855eda4eeb174fec30b18bd
Yusef Mobasheri <yusef.mobasheri@gmail.com>
2017-02-09 10:32:59 +0330

    create webservice finished

--- protected/config/database.php
+++ protected/config/database.php
@@ -11,7 +11,7 @@ return array(
 	'username' => 'avayesha_gm',
 	'password' => '72k~l~y_9gEysp8zJI',
 	'charset' => 'utf8',*/
-	'connectionString' => 'mysql:host=localhost;dbname=gohar_app',
+	'connectionString' => 'mysql:host=localhost;dbname=gohar-app',
 	'tablePrefix'=>'iw_',
 	'emulatePrepare' => true,
 	'username' => 'root',
