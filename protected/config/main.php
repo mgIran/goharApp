fd7791c6141c97127da527d0272c884ebee9c054
moeinsr <4mohammadmoien@gmail.com>
2018-02-22 18:16:53 +0330

    Fix bugs

--- protected/config/main.php
+++ protected/config/main.php
@@ -19,7 +19,7 @@ return array(
 	),
 
 	'modules'=>array(
-		// uncomment the following to enable the Gii tool
+		//  uncomment the following to enable the Gii tool
 
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
